390c9b2f3f46c763cfed24e084d71a50a799cdd5
zx64 <zx64@users.noreply.github.com>
2026-06-12 20:53:08 +0100

    Encapsulate native direct816 module with Python module.

--- modules/c/direct816/direct816_bindings.c
+++ modules/c/direct816/direct816_bindings.c
@@ -2,7 +2,7 @@
 
 /* Module Globals */
 static const mp_map_elem_t direct816_globals[] = {
-    { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_direct816) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR__direct816) },
 };
 static MP_DEFINE_CONST_DICT(mp_module_direct816_globals, direct816_globals);
 
@@ -11,4 +11,4 @@ const mp_obj_module_t direct816_user_cmodule = {
     .globals = (mp_obj_dict_t*)&mp_module_direct816_globals,
 };
 
-MP_REGISTER_MODULE(MP_QSTR_direct816, direct816_user_cmodule);
+MP_REGISTER_MODULE(MP_QSTR__direct816, direct816_user_cmodule);
